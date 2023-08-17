68b1e5ef9f3094e630579715010216f15d6d8664
mepha89 <umamarahman12@gmail.com>
2023-08-16 23:39:16 -0400

    create-authpage-background

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import Navbar from "@/components/Navbar";
+import React from "react";
 
-type AuthPageProps = {
-    
+type AuthPageProps = {};
+
+const page: React.FC<AuthPageProps> = () => {
+  return (
+    <div className="bg-gradient-to-b from-gray-600 h-screen relative">
+      <div className="max-w-7xl mx-auto">
+        <Navbar />
+      </div>
+    </div>
+  );
 };
-
-const page:React.FC<AuthPageProps> = () => {
-    
-    return <div>This is our auth page</div>
-}
 export default page;
