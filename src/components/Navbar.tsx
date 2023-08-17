5e45bdf42b4b6f4873062ffac9d6b28cc37e799b
mepha89 <umamarahman12@gmail.com>
2023-08-16 23:48:08 -0400

    create-sign-in-button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
-type NavbarProps = {
-    
+type NavbarProps = {};
+
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
+    <div className="flex items-center justify-between sm:px-12 px-2 md:px-24">
+      <Link href="/" className="flex items-center justify-center h-20">
+        <img src="/logo.png" alt="LeetClone" className="h-full" />
+      </Link>
+      <div className="flex item-center">
+        <button
+          className="bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
+                hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange
+                "
+        >
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
 };
-
-const Navbar:React.FC<NavbarProps> = () => {
-    
-    return <div>Navbar</div>
-}
 export default Navbar;
